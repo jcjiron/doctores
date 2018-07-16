098cee26a9b3b45415f720e190e3235f4369cb61
Juan Carlos <jironjuarezjuan@hotmail.com>
2018-07-15 20:12:42 -0500

    feature(register): adding preview of photo

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -6,6 +6,8 @@ import { Observable } from 'rxjs';
 import { Doctor } from '../../model/doctor.interface';
 import { HttpLocalService } from '../../core/http-local.service';
 
+import * as Pattern from '../../shared/utils/app.patterns';
+
 declare var $: any;
 declare var Materialize: any;
 
@@ -19,56 +21,78 @@ export class RegisterComponent implements OnInit {
   private doctor:Doctor;
   private doctorForm: FormGroup;
   private specialties: any[] = [];
+  private photo:string = '';
+  private photoTemp:File;
+  private photoUpload:File;
 
   constructor(
     private fs: FirebaseService,
     private lss: LocalStorageService,
-  private httpLocal: HttpLocalService) {
+    private httpLocal: HttpLocalService) {
 
-    this.doctor = { address: '', certificate: '', email: '', name: '', phone: '', photo: '', speciality: '', token: ''}
+      this.doctor = { address: '', certificate: '', email: '', name: '', phone: '', photo: '', speciality: '', token: ''}
 
 
 
-    this.doctorForm = new FormGroup({
-      'name': new FormControl(null, [Validators.required]),
-      'phone': new FormControl(null, [Validators.required, Validators.pattern('^\d{10}$')]),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'specialty': new FormControl(null, [Validators.required]),
-      'certificate': new FormControl(null, [Validators.required]),
-      'address': new FormControl(null, [Validators.required])
+      this.doctorForm = new FormGroup({
+        'name': new FormControl(null, [Validators.required]),
+        'phone': new FormControl(null, [Validators.required, Validators.pattern(Pattern.PHONE)]),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'specialty': new FormControl(null, [Validators.required]),
+        'certificate': new FormControl(null, [Validators.required, Validators.pattern(Pattern.CERTIFICATE)]),
+        'address': new FormControl(null, [Validators.required])
 
-    }, {updateOn: 'blur'});
+      }, {updateOn: 'blur'});
+    }
+
+    ngOnInit() {
+
+      const user = this.lss.getItem('user') ? JSON.parse(this.lss.getItem('user')) : null;
+
+      this.fs.getDoctorByEmail(user.email)
+      .subscribe((data:any[])=>{
+        this.doctor = data.length ? data[0] : null;
+
+        // edit user information
+        if (this.doctor) {
+
+        // else register user
+      }else{
+      this.doctorForm.controls['email'].setValue(user.email);
+      this.doctorForm.controls['email'].disable();
+      this.doctorForm.controls['name'].setValue(user.displayName);
+      // console.log(user)
+      this.photo = user.photoURL;
+      Materialize.updateTextFields();
+    }
+  });
+
+  this.httpLocal.get('specialties.json')
+  .subscribe((data:any)=>{
+    this.specialties = data.specialties;
+  });
+}
+
+loadPhoto(file:File){
+  if (file) {
+    if (file.type.indexOf('image') >= 0) {
+
+      this.photoUpload = file;
+
+      let reader = new FileReader();
+      let urlPhotoTemp = reader.readAsDataURL( file );
+
+      reader.onloadend = ()=> this.photoTemp = reader.result;
+    }
   }
+}
 
-  ngOnInit() {
+sendForm(){
+  console.log(this.doctorForm);
+}
 
-    const user = this.lss.getItem('user') ? JSON.parse(this.lss.getItem('user')) : null;
-
-    this.fs.getDoctorByEmail(user.email)
-    .subscribe((data:any[])=>{
-      this.doctor = data.length ? data[0] : null;
-
-          this.doctorForm.controls['email'].setValue(user.email);
-          this.doctorForm.controls['email'].disable();
-
-          this.doctorForm.controls['name'].setValue(user.displayName);
-          Materialize.updateTextFields();
-    });
-
-    this.httpLocal.get('specialties.json')
-    .subscribe((data:any)=>{
-      this.specialties = data.specialties;
-    });
-
-  }
-
-  sendForm(){
-    console.log(this.doctorForm.value);
-  }
-
-  change(event){
-    // console.log(event.target.value)
-    this.doctorForm.controls['specialty'].setValue(event.target.value);
-  }
+changeSelect(event){
+  this.doctorForm.controls['specialty'].setValue(event.target.value);
+}
 
 }
---
+++ src/app/shared/utils/app.patterns.ts
@@ -0,0 +1,4 @@
+
+
+export const PHONE = '^[0-9]{10}$';
+export const CERTIFICATE = '^[0-9]{7}$';
